a3370fabd282b2515da4f923e11d50b2ea4300d3
itsrusty <rusty3031@gmail.com>
2023-12-24 01:51:48 -0700

    unit-cars controller addeds

---
+++ src/controllers/units-cars/units-cars.controllers.ts
@@ -0,0 +1,68 @@
+import { Request, Response } from "express";
+import unitModel from "../../models/units.model";
+
+class unitCarsController {
+  async getCarsUnit(req: Request, res: Response) {
+    try {
+      const renderCarUnits = await unitModel.find();
+
+      renderCarUnits
+        ? res.status(200).json({ message: renderCarUnits, details: true })
+        : res.status(500).json({ messageError: "error", details: false });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async createCarUnit(req: Request, res: Response) {
+    try {
+      const { marca, modelo, lastOilChange, nextOilChange } = req.body;
+
+      const dataCarUnitReceived = {
+        marca: marca,
+        modelo: modelo,
+        lastOilChange: lastOilChange,
+        nextOilChange: nextOilChange,
+      };
+
+      const saveUnitCarInDatabase = await unitModel.create(dataCarUnitReceived);
+
+      saveUnitCarInDatabase
+        ? res
+            .status(200)
+            .json({ message: saveUnitCarInDatabase, details: true })
+        : res
+            .status(500)
+            .json({ messageError: "error internal", details: false });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async editCarUnit(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      // const { }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async deleteCarUnit(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+
+      const deleteUnitById = await unitModel.findByIdAndDelete(id);
+
+      deleteUnitById
+        ? res.status(200).json({ message: "deleted", detials: deleteUnitById })
+        : res
+            .status(404)
+            .json({ messageError: "error internal", details: false });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+export default unitCarsController;
---
+++ src/models/units.model.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const unitModelCar = new mongoose.Schema({
+  marca: {
+    type: String,
+    required: true,
+  },
+
+  modelo: {
+    type: Number,
+    required: true,
+  },
+
+  lastOilChange: {
+    type: Date,
+    required: true,
+  },
+
+  nextOilChange: {
+    type: Date,
+    required: true,
+  },
+});
+
+const unitModel = mongoose.model("unitCars", unitModelCar);
+
+export default unitModel;
