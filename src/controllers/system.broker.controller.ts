554649238a7e2d438a630cab38e9ef05a09be44e
itsrusty <rusty3031@gmail.com>
2024-01-07 14:27:39 -0700

    filter products by your tag

--- src/controllers/system.broker.controller.ts
+++ src/controllers/system.broker.controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import employeeModel from "../models/employees.model";
+import { productMarks } from "../services/employees/models/products";
 
 class systemBroker {
   async closeCourt(req: Request, res: Response) {
@@ -12,6 +14,37 @@ class systemBroker {
   async viewHistoryCourt(req: Request, res: Response) {
     try {
       const { id } = req.params;
+      console.log("id input: " + id);
+
+      // interface Product {
+      //   productFinish: boolean
+      // }
+
+      const products = await productMarks.find();
+      const queryUserInfo = await employeeModel.find({ _id: id });
+
+      // const filterProductsByFinish = (products: Product[], isProductFinish: Product[]) =>
+      //   products.filter((product: any) => product.productFinish === isProductFinish);
+
+      // // ? filter products sold and not solds!
+      // const filterSoldProducts = filterProductsByFinish(products, true);
+      // const filterUnsoldProducts = filterProductsByFinish(products, false);
+
+      const filterSoldProducts = products.filter(
+        (productsBuy) => productsBuy.productFinish === true
+      );
+      const filterUnsoldProducts = products.filter(
+        (unsoldProducts) => unsoldProducts.productFinish === false
+      );
+
+      queryUserInfo
+        ? res.status(200).json({
+            response: "found",
+            userInfo: queryUserInfo,
+            soldProducts: filterSoldProducts,
+            unsoldProducts: filterUnsoldProducts,
+          })
+        : res.status(404).json({ response: "not found" });
 
       // const userInfo = { } // información del empleado al que se le quiere ver la info --> { username, vehicleAssignament, stateRoute },
 
@@ -21,7 +54,6 @@ class systemBroker {
       // const estimatedPrices = //descripción de --> salió con "$16mil pesos", vendió "$13400 pesos", entregó en efectivo "$5mil pesos", entregó en mercancia "$900 pesos"
 
       // const differenceCount = // la diferencia de dinero es "$10 pesos"
-      
     } catch (error) {
       console.error(error);
     }
