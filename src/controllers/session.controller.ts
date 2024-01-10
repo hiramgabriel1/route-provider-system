b29523ab2de8d89febdd280a6f9448cb68cef08c
itsrusty <rusty3031@gmail.com>
2024-01-09 23:44:18 -0700

    change logic to controller session validarion

--- src/controllers/session.controller.ts
+++ src/controllers/session.controller.ts
@@ -2,44 +2,18 @@ import { Request, Response } from "express";
 import employeeModel from "../models/employees.model";
 
 class sessionController {
-  // async createNewSessionUser(req: Request, res: Response) {
-  //   try {
-  //     const { username, role, password } = req.body;
-
-  //     // todo: encrypt password!
-
-  //     const encrypt =  await encryptPasswordSecurity(password);
-
-  //     const dataSessionUser = {
-  //       username: username,
-  //       role: role,
-  //       password: encrypt,
-  //     };
-
-  //     console.log(dataSessionUser);
-
-  //     // todo: save query in database
-  //     const saveSession = await session.create(dataSessionUser);
-
-  //     saveSession
-  //       ? res.status(200).json({ response: saveSession })
-  //       : res.status(500).json({ response: false });
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // }
-
   async validateSessionInput(req: Request, res: Response) {
     try {
       const { username, role } = req.body;
-      const verifySessionAndTypeRole = await employeeModel
-        .find({
-          username: username,
-          role: role,
-        })
+      const verifySessionAndTypeRole = await employeeModel.find({
+        username: username,
+        role: role,
+      });
 
-        if(verifySessionAndTypeRole) return res.status(200).json({ message: "user found" , response: verifySessionAndTypeRole })
-        
+      if (verifySessionAndTypeRole)
+        return res
+          .status(200)
+          .json({ message: "user found", response: verifySessionAndTypeRole });
 
       // verifySessionAndTypeRole.length > 0
       //   ? res.json({ response: "user found", data: verifySessionAndTypeRole })
