57e24b05e3f9c45c54e2891e45560d7920646bc1
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-01-16 09:56:59 -0300

    resolve error un validate session

--- src/controllers/session.controller.ts
+++ src/controllers/session.controller.ts
@@ -5,28 +5,29 @@ import { verifyPasswordSecurity } from "../validators/bcrypt.config";
 class sessionController {
   async validateSessionInput(req: Request, res: Response) {
     try {
-      const { username, role, password } = req.body;
-      const verifySessionAndTypeRole = await employeeModel.find({
-        username: username,
-        role: role,
-      });
-      let isValid = false;
-      verifySessionAndTypeRole
-        ? (isValid =password==verifySessionAndTypeRole[0].password)
-        : res.status(400).json({ message: "user not found", details: false });
+        const { username, role, password } = req.body;
+        const verifySessionAndTypeRole = await employeeModel.find({
+            username: username,
+            role: role,
+        });
 
-      isValid
-        ? res
-            .status(200)
-            .json({ message: "user found", response: verifySessionAndTypeRole })
-        : res
-            .status(400)
-            .json({ message: "Contrasenia incorrecta", details: false });
+        if (verifySessionAndTypeRole.length > 0) {
+            const isValid = password === verifySessionAndTypeRole[0].password;
+
+            if (isValid) {
+                res.status(200).json({ message: "user found", response: verifySessionAndTypeRole });
+            } else {
+                res.status(400).json({ message: "Incorrect password", details: false });
+            }
+        } else {
+            res.status(400).json({ message: "User not found", details: false });
+        }
     } catch (error) {
-      res.status(500).json({ messageError: error });
-      console.error(error);
+        res.status(500).json({ messageError: error });
+        console.error(error);
     }
-  }
+}
+
 }
 
 export default sessionController;
