31a71f288eebb958d7b9e5ef30154341273ac33c
itsrusty <rusty3031@gmail.com>
2024-01-30 08:24:57 -0700

    remove utils and other

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,3 @@
-// import morgan from "morgan";
 import express, { Express } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -15,8 +14,6 @@ import routerBrokerCourt from "../routes/system.broker.router";
 import routerMarkProducts from "../services/employees/routes/products.routes";
 import session from "express-session";
 import morgan from "morgan";
-import swaggerUi from "swagger-ui-express";
-// import specs from "../doc/swagger";
 
 dotenv.config();
 connection();
@@ -56,10 +53,6 @@ app.use(routerBrokerCourt);
 
 // & endpoints employees here
 app.use(routerMarkProducts);
-
-// & endpoint to documentation api here
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
-// app.use("api-docs", SwaggerUiOptions, swaggerUi.setup(specs));
 
 // todo: run server!
 const bootstrap = () => {
