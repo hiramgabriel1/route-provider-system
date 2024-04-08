38e8dc21265b2915072c7f1a170dd073b1cb4d3b
Addmer (cris) <cristhianalexand@gmail.com>
2024-04-08 09:54:19 -0500

    Adelanto de funcionalidad

---
+++ src/types/requestProducts.d.ts
@@ -0,0 +1,19 @@
+export interface RequestProductsInterface {
+  _id: string;
+  state: string;
+  dateTime: string;
+  route: string;
+  products: Product[];
+  __v: number;
+  assignedQuantity?: number;
+}
+
+export interface ProductInRequestProductsInterface {
+  assignedQuantity: number;
+  product: string;
+  amount: number;
+  amountCurrent: number;
+  unitPrice: number, //added
+  priceSold: number //added
+  _id: string;
+}
